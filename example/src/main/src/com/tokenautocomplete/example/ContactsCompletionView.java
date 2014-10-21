5f4b4c05924f1d691c630243f15e40c0eb6776bd
Anila <anila.vasudevan@inapp.com>
2014-10-21 11:43:42 +0530

    This is first commit

---
+++ example/src/main/src/com/tokenautocomplete/example/ContactsCompletionView.java
@@ -0,0 +1,54 @@
+package com.tokenautocomplete.example;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.tokenautocomplete.sample.TokenCompleteTextView;
+
+
+public class ContactsCompletionView extends TokenCompleteTextView {
+
+    public ContactsCompletionView(Context context) {
+        super(context);
+    }
+
+    public ContactsCompletionView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ContactsCompletionView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @SuppressLint("NewApi") @Override
+    protected View getViewForObject(Object object) {
+        Person p = (Person)object;
+        TextView name;
+        LayoutInflater l = (LayoutInflater)getContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        LinearLayout view = (LinearLayout)l.inflate(R.layout.contact_token, (ViewGroup)ContactsCompletionView.this.getParent(), false);
+        name = (TextView) view.findViewById(R.id.name);
+        name.setCompoundDrawablesWithIntrinsicBounds(R.drawable.user,
+				0, 0, 0);
+        name.setText(p.getEmail());
+        
+        return view;
+    }
+
+    @Override
+    protected Object defaultObject(String completionText) {
+        //Stupid simple example of guessing if we have an email or not
+        int index = completionText.indexOf('@');
+        if (index == -1) {
+            return new Person(completionText, completionText.replace(" ", "") + "@example.com");
+        } else {
+            return new Person(completionText.substring(0, index), completionText);
+        }
+    }
+}
